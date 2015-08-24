e6a693f263433f670e0b0bba3474fef03e3ed901
Mircea Tanasă <tanasamircea@yahoo.com>
2015-08-24 10:37:36 +0300

    *Added services page

--- page-templates/page_services.php
+++ page-templates/page_services.php
@@ -8,48 +8,24 @@
         Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum. Duis mollis, non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh.
       </p>
     </div>
+    
+    <?php $args = array( 'post_type' => 'services', 'posts_per_page' => 10 );
+      $loop = new WP_Query( $args );
+      while ( $loop->have_posts() ) : $loop->the_post();
+        the_title();
+        echo '<div class="entry-content">';
+        the_content();
+        echo '</div>';
+      endwhile;
+      ?>
+
     <div class="text-box">
       <div class = "text-box_image"><img src="lib/gui-icons/pen.png" alt="pen"></div>
       <div class="text-box_content">
         <h2>Custom Design</h2>
         <p>Curabitur vestibulum eget mauris quis laoreet. Phasellus in quam laoreet, viverra lacus ut, ultrices velit.</p>
       </div>
-    </div>
-    <div class="text-box">
-      <div class = "text-box_image"><img src="lib/gui-icons/bulb.png" alt="idea bulb"></div>
-      <div class="text-box_content">
-        <h2>Inovative Ideas</h2>
-        <p>Quisque luctus, quam eget molestie commodo, lacus purus cursus purus, nec rutrum tellus dolor id lorem.</p>
       </div>
-    </div>
-    <div class="text-box">
-      <div class = "text-box_image"><img src="lib/gui-icons/heart.png" alt="heart"></div>
-      <div class="text-box_content">
-        <h2>Love Is The Answer</h2>
-        <p>Nulla sed nunc et tortor luctus faucibus. Morbi at aliquet turpis, et consequat felis. Quisque condimentum.</p>
-      </div>
-    </div>
-    <div class="text-box">
-      <div class = "text-box_image"><img src="lib/gui-icons/phone.png" alt="phone"></div>
-      <div class="text-box_content">
-        <h2>Responsive Layout</h2>
-        <p>Sed porttitor placerat rhoncus. In at nunc tellus. Maecenas blandit nunc ligula. Praesent elit leo.</p>
-      </div>
-    </div>
-    <div class="text-box">
-      <div class = "text-box_image"><img src="lib/gui-icons/bubble.png" alt="bubble"></div>
-      <div class="text-box_content">
-        <h2>24 / 7 Support</h2>
-        <p>Vivamus vel quam lacinia, tincidunt dui non, vehicula nisi. Nulla a sem erat. Pellentesque egestas venenatis lorem .</p>
-      </div>
-    </div>
-    <div class="text-box">
-      <div class = "text-box_image"><img src="lib/gui-icons/star.png" alt="star"></div>
-      <div class="text-box_content">
-        <h2>Feel Like A Star</h2>
-        <p>Quisque hendrerit purus dapibus, ornare nibh vitae, viverra nibh. Fusce vitae aliquam tellus.</p>
-      </div>
-    </div>
-  </div>
+
 </div>
 <?php get_footer() ?>
