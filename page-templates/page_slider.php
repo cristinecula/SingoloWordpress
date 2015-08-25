9d602ef6c441336e95aec377c5e346f2f5bb70f8
Mircea Tanasă <tanasamircea@yahoo.com>
2015-08-25 11:41:48 +0300

    *The slider takes the slides from wordpress

--- page-templates/page_slider.php
+++ page-templates/page_slider.php
@@ -1,22 +1,27 @@
 <?php /* Template Name: Slider */ ?>
 <?php get_header(); ?>
+<script src="<?php echo get_bloginfo('template_directory');?>/scripts/slider.js"></script>
+
 
 
 <div class="slideshow" id="slide-1">
+
   <div class="slideshow_container">
-    <div class="slideshow_slide">
-      <img class="slideshow_banner" src='<?php echo get_bloginfo('template_directory');?>/images/slide.png' />
-    </div>
-    <div class="slideshow_slide">
-      <img class="slideshow_banner" src='<?php echo get_bloginfo('template_directory');?>/images/slide.png' />
-    </div>
-    <div class="slideshow_slide">
-      <img class="slideshow_banner" src='<?php echo get_bloginfo('template_directory');?>/images/slide.png'/>
-    </div>
+
+    <?php $args = array( 'post_type' => 'slides' );
+      $loop = new WP_Query( $args );
+      while ( $loop->have_posts() ) : $loop->the_post();
+      $image = wp_get_attachment_image_src( get_post_thumbnail_id( $post->ID ), 'single-post-thumbnail' );
+
+        echo '<div class="slideshow_slide">
+                 <img class="slideshow_banner" src= " '; echo $image[0]; echo'" />
+              </div>';
+      endwhile;
+    ?>
+
   </div>
   <img src="<?php echo get_bloginfo('template_directory');?>/images/next.png" class="btn-next" />
   <img src="<?php echo get_bloginfo('template_directory');?>/images/prev.png" class="btn-prev" />
 </div>
-<script src="<?php echo get_bloginfo('template_directory');?>/scripts/slider.js"></script>
 
 <?php get_footer(); ?>
