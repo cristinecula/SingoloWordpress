1bfcad84cd1ed83e42ba91ff25e5a01fe1620b7c
Mircea Tanasă <tanasamircea@yahoo.com>
2015-08-26 10:51:06 +0300

    *Added social media buttons in footer *Customizable logo in header

--- footer.php
+++ footer.php
@@ -1,6 +1,12 @@
       <div class="footer">
         <div class="midpage">
-          <span>&#169; Copyright <?php the_time('Y'); ?> &#8226; by PSDchat.com<</span>
+          <span>&#169; Copyright <?php the_time('Y'); ?> &#8226; by <?php echo get_site_url(); ?></span>
+          <div class="social-media">
+            <a href = "http://www.facebook.com"><img src="http://localhost/wp-content/uploads/2015/08/facebook.png" alt=""></a>
+            <a href = "http://www.google.com"><img src="http://localhost/wp-content/uploads/2015/08/google+.png" alt=""></a>
+            <a href = "http://www.twitter.com"><img src="http://localhost/wp-content/uploads/2015/08/twitter.png" alt=""></a>
+            <a href = "http://www.linkedin.com"><img src="http://localhost/wp-content/uploads/2015/08/linkedin.png" alt=""></a>
+          </div>'
         </div>
       </div>
   </body>
--- header.php
+++ header.php
@@ -5,12 +5,19 @@
   <link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>">
   <link href='http://fonts.googleapis.com/css?family=Lato&subset=latin,latin-ext' rel='stylesheet' type='text/css'>
    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+  <script src="<?php echo get_bloginfo('template_directory');?>/scripts/responsive.js"></script>
 </head>
 <body>
     <div class="header">
       <div class="midpage">
         <div class="header_logo">
-          <a href="<?php bloginfo('url'); ?>"> <?php bloginfo('name'); ?> </a>
+        <?php 
+        $logo = get_bloginfo();
+        $begin = substr($logo, 0,strlen($logo)-1);
+        $final = substr($logo, -1);
+         ?>
+          <a href="<?php bloginfo('url'); ?>"><?php echo $begin; ?>  </a>
+          <span><?php echo $final ?></span>
         </div>
          <?php wp_nav_menu( array( 'container_class' => 'main-nav', 'theme_location' => 'primary' ) ); ?>
       </div>
