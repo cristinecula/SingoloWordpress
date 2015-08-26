2e60d6e107257187bf6ace3042e49b28223575c8
Mircea Tanasă <tanasamircea@yahoo.com>
2015-08-26 10:52:40 +0300

    *Fixed bug where page title and description wasn't shown

--- page-templates/page_portfolio.php
+++ page-templates/page_portfolio.php
@@ -3,34 +3,23 @@
     <script>
       window.projects =[
       <?php $args = array( 'post_type' => 'projects', 'posts_per_page' => 20  );
+      $page_title = $wp_query->post->post_title;
+      $description = get_post_meta($post->ID, 'Portfolio', true);
       $loop = new WP_Query( $args );
-
       while ( $loop->have_posts() ) : $loop->the_post();
       $image = wp_get_attachment_image_src( get_post_thumbnail_id( $post->ID ), 'single-post-thumbnail' );
-      
       $term_list =  wp_get_post_terms($post->ID, 'type', array("fields" => "names"));
       $js_array = json_encode($term_list);
 
-
-
-
-
-
         echo "{
-        id: 1,
+        id: "; echo $post->ID; echo ",
         name: '"; the_title(); echo " ',
         photo: '"; echo $image[0]; echo" ',
         info: '"; echo get_the_content(); echo "',
         categories:"; echo $js_array; echo "
       },";
-
-        
-
       endwhile;
       ?>
-
-
-
       ];
     </script>
     <script src="<?php echo get_bloginfo('template_directory');?>/scripts/app.js"></script>
@@ -43,9 +32,8 @@
 <div class="portofolio" id="portofolio">
   <div class="midpage">
     <div class="page_title">
-      <h1 > <?php echo get_the_title(); ?> </h1>
-      <p><?php echo get_post_meta($post->ID, 'Portfolio', true);; ?></p>
-      </p>
+      <h1><?php echo $page_title; ?></h1>
+      <p><?php echo $description; ?></p>
     </div>
       <div class="displayer">
       </div>
